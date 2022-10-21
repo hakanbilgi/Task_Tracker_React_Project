36560279891e949e9ce38b16b80009814a1a5bdd
Hakan Bilgi <hknblg34@gmail.com>
2022-10-21 16:17:27 +0300

    rejenere

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import TaskPng from "../../assets/TaskPng";
 
 const Header = () => {
@@ -6,7 +7,6 @@ const Header = () => {
       <TaskPng />
       <h1 className="display-5">TASK TRACKER</h1>
     </div>
- 
   );
 };
 export default Header;
